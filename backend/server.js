ad60430cfb8ae4c8a91a3fa69db90d1e396e37c3
mustafapress <ultimatem.dev@gmail.com>
2026-01-05 04:35:58 +0500

    chore: configure for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -54,4 +54,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+}
+
+export default app;
